b91574a0c86eded28ff2a57b2d69eca91cc02bb4
nyomanjyotisa <jyotisa1616@gmail.com>
2021-04-21 16:00:42 +0800

    basic admin teams

--- app/Http/Controllers/PubgmController.php
+++ app/Http/Controllers/PubgmController.php
@@ -12,4 +12,27 @@ class PubgmController extends Controller
         $teams = Pubgm::all();
         return view('admin/pubgm', ['teams' => $teams]);
     }
+
+    public function detail($id){
+        $players = PubgmPlayer::where('team_id', $id)->get();
+        $team = Pubgm::find($id);
+        $lomba = 'pubgm';
+        return view('admin/detail', ['players' => $players, 'team' => $team, 'lomba' => $lomba]);
+    }
+
+        // hapus sementara
+    public function delete($id)
+    {
+        $team = Pubgm::find($id);
+        $team->delete();
+
+        return redirect('/admin/pubgm');
+    }
+
+    public function setpaid($id){
+        $team = Pubgm::find($id);
+        $team->status = 'paid';
+        $team->save();
+        return redirect()->back();
+    }
 }
--- app/Http/Controllers/ValorantController.php
+++ app/Http/Controllers/ValorantController.php
@@ -12,4 +12,27 @@ class ValorantController extends Controller
         $teams = Valorant::all();
         return view('admin/valorant', ['teams' => $teams]);
     }
+
+    public function detail($id){
+        $players = ValorantPlayer::where('team_id', $id)->get();
+        $team = Valorant::find($id);
+        $lomba = 'valorant';
+        return view('admin/detail', ['players' => $players, 'team' => $team, 'lomba' => $lomba]);
+    }
+
+        // hapus sementara
+    public function delete($id)
+    {
+        $team = Valorant::find($id);
+        $team->delete();
+
+        return redirect('/admin/valorant');
+    }
+
+    public function setpaid($id){
+        $team = Valorant::find($id);
+        $team->status = 'paid';
+        $team->save();
+        return redirect()->back();
+    }
 }
